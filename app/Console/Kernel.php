ebb3f4da562508f36396c6e37c1c9b5b4d54c419
Daniel Neto <danielneto314@gmail.com>
2024-02-26 22:57:07 -0300

    chore: instala e configura o pacote 'Laravel Backup'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,17 +7,13 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:run')
+            ->everyMinute()
+            ->environments('production');
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__ . '/Commands');
--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,16 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+
+        'backup' => [
+            'driver' => 's3',
+            'key' => env('AWS_BACKUP_ACCESS_KEY_ID'),
+            'secret' => env('AWS_BACKUP_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_BACKUP_DEFAULT_REGION'),
+            'bucket' => env('AWS_BACKUP_BUCKET'),
+            'url' => env('AWS_BACKUP_URL'),
+            'endpoint' => env('AWS_BACKUP_ENDPOINT'),
+        ],
     ],
 
     /*
