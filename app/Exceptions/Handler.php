d0d4c3cebc461a48d2637a483eb79740acc9a4d1
Daniel Neto <danielneto314@gmail.com>
2024-03-05 16:48:16 -0300

    chore: instala e configura o pacote 'Sentry Laravel'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,27 +3,21 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e): void {
+        $this->reportable(function (Throwable $throwable): void {
+            Integration::captureUnhandledException($throwable);
         });
     }
 }
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,15 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+import * as Sentry from "@sentry/browser";
 import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
