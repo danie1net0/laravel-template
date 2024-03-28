9d6a74d9c923499e6ffa4308412ed5c3723bb5f5
Daniel Neto <danielneto314@gmail.com>
2024-03-28 20:22:54 -0300

    feat(create_users_table): adiciona novas colunas na tabela 'users'
    adiciona as colunas 'roles', 'permissions' e 'is_active' na tabela 'users'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,9 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table): void {
@@ -16,14 +13,14 @@ return new class () extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->json('roles')->default(json_encode([]));
+            $table->json('permissions')->default(json_encode([]));
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
