f3d6bb381148ab54c2f0b0a04a405941b62e832b
Daniel Neto <danielneto314@gmail.com>
2024-03-28 22:21:50 -0300

    refactor(user): refatora o modelo 'User'
    substitui o atributo '$casts' pelo método 'casts()' no modelo 'User'

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,11 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
