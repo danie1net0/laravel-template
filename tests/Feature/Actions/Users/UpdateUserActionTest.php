1f1e3b7261f3c482b410d57aa8bb63dee38668b4
Daniel Neto <danielneto314@gmail.com>
2024-03-30 19:35:08 -0300

    feat(user actions): implementa ação para editar usuário
    implementa ação 'app/Actions/Users/UpdateUserAction.php'

---
+++ tests/Feature/Actions/Users/UpdateUserActionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Actions\Users\UpdateUserAction;
+use App\Enums\Role;
+use App\Models\User;
+
+use function Pest\Laravel\assertDatabaseHas;
+
+uses()->group('actions', 'users');
+
+it('deve editar usuário', function (bool $isActive): void {
+    $user = User::factory()->create();
+
+    $input = User::factory()
+        ->make([
+            'roles' => $roles = [Role::ADMIN],
+            'is_active' => $isActive,
+        ]);
+
+    (new UpdateUserAction())->execute($user, $input->toArray());
+
+    expect($user->roles)->toEqual(collect($roles));
+
+    assertDatabaseHas(User::class, [
+        'id' => $user->id,
+        'name' => $input['name'],
+        'email' => $input['email'],
+        'is_active' => $input['is_active'],
+    ]);
+})->with([true, false]);
+
+it('deve editar usuário sem todos parâmetro', function (string $param): void {
+    $user = User::factory()->create();
+
+    $input = User::factory()
+        ->set('roles', [Role::ADMIN])
+        ->make();
+
+    $originalValue = $user->{$param};
+
+    unset($input[$param]);
+
+    (new UpdateUserAction())->execute($user, $input->toArray());
+
+    expect($user->fresh()->{$param})->toEqual($originalValue);
+})->with(['name', 'email', 'roles', 'is_active']);
