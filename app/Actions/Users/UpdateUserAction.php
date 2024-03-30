3062eaad78a5f5d2e32ac6a5670ccef66730476f
Daniel Neto <danielneto314@gmail.com>
2024-03-30 19:35:08 -0300

    feat(user actions): implementa ação para editar usuário
    implementa ação 'app/Actions/Users/UpdateUserAction.php'

---
+++ app/Actions/Users/UpdateUserAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Actions\Users;
+
+use App\Models\User;
+
+class UpdateUserAction
+{
+    public function execute(User $user, array $input): void
+    {
+        $user->update([
+            'name' => $input['name'] ?? $user->name,
+            'email' => $input['email'] ?? $user->email,
+            'roles' => $input['roles'] ?? $user->roles,
+            'is_active' => $input['is_active'] ?? $user->is_active,
+        ]);
+    }
+}
