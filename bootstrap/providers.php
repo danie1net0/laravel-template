0b57809936aafeee977e6489c7327cfbc02b347e
Daniel Neto <danielneto314@gmail.com>
2024-03-30 18:48:47 -0300

    fix(providers): remove declaração de provider inexistente
    remove a declaração do provider 'App\Providers\Filament\AdminPanelProvider::class' do arquivo
    'bootstrap/providers.php'

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
